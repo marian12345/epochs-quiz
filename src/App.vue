8d59f22466856c800dfea3c51f8126f09dd06c3a
marian12345 <marian.zwarg@gmx.de>
2026-02-23 16:55:40 +0100

    add card

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CardComponent from '@/components/CardComponent.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentationas
-  </p>
+  <CardComponent />
 </template>
 
 <style scoped></style>
